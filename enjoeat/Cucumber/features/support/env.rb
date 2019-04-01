1e68425eb7ce955b2a6acea2639d4d249075cac6
Rafael Carvalho <rapha.pse@outlook.com>
2019-04-01 14:27:44 -0300

    Atualizações nos teste com Cucumber, Capybara, Selenium e Ruby

--- enjoeat/Cucumber/features/support/env.rb
+++ enjoeat/Cucumber/features/support/env.rb
@@ -4,6 +4,6 @@ require 'selenium-webdriver'
 
 Capybara.configure do |x|
     x.default_driver = :selenium_chrome
-    x.app_host = 'http://enjoeat-sp1.herokuapp.com'
+    x.app_host = 'http://enjoeat-sp3.herokuapp.com'
     x.default_max_wait_time = 10
 end
